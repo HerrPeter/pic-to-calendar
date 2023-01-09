349346d81bc6d7ccaa848d9eb1112ae67ffa88e1
Peter Kaufinger <34880135+HerrPeter@users.noreply.github.com>
2023-01-09 14:31:12 -0800

    TS convert for new class

--- ts-output/recognizer.js
+++ ts-output/recognizer.js
@@ -1,1 +1,27 @@
 "use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+var googleapis_1 = require("googleapis");
+var Recognizer = /** @class */ (function () {
+    function Recognizer(textLines) {
+        var _this = this;
+        this.selectedCalendar = '';
+        this.getNextEvent = function () {
+            if (_this.scheduleLines.length == 0) {
+                return null;
+            }
+        };
+        this.addToCalendar = function (event) {
+            // Note: Selected Calendar needs to be a user's calendar ID
+            // Note: Request Body supplies the info for the new event to be added
+            googleapis_1.google
+                .calendar('v3')
+                .events.insert({
+                calendarId: _this.selectedCalendar,
+                requestBody: { start: {}, end: {} },
+            });
+        };
+        this.scheduleLines = textLines;
+    }
+    return Recognizer;
+}());
+exports.default = Recognizer;
